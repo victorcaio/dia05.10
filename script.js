fcb8568c125fccc5b31b462bc5496634625fe6a6
victorcaio <146743625+victorcaio@users.noreply.github.com>
2024-09-07 12:52:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+const password = 'cristina'; // Replace with your desired password
+const frase = "Sei que fui estúpido e não mereço desculpas, mas não desista de mim. Poxa, gosto de você e por isso fiquei com raiva. Poderia me dar uma nova chance? Você pode mudar esse meu lado ruim!";
+const elementoFrase = document.getElementById("frase");
+let indice = 0;
 
+window.addEventListener('load', function() {
+  const passwordPrompt = prompt('Enter password to access the page:');
+  
+  if (passwordPrompt === password) {
+    // Remove the "not-loaded" class to show the content
+    document.body.classList.remove("not-loaded");
+    escreverLetra();
+  } else {
+    // Redirect to Google if password is incorrect
+    window.location.href = 'https://www.google.com';
+  }
+});
 
-function mostra(el){
-    document.getElementById(el).style.display ='block'
-    
-    }
-
-
-
-function esconde(el){
-document.getElementById(el).style.display ='none'
-
+function escreverLetra() {
+  if (indice < frase.length) {
+    elementoFrase.textContent += frase[indice];
+    indice++;
+    setTimeout(escreverLetra, 300); // tempo de espera entre letras
+  }
 }
-
-   
